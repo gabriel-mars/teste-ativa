585922d96f26759bc3d2facb36e8a0713ca7767e
Gabriel Martins <gabrielrms@outlook.com.br>
2021-05-24 22:43:51 -0300

    Correcao no DDL

--- DDL_Teste_ATIVA.sql
+++ DDL_Teste_ATIVA.sql
@@ -23,11 +23,17 @@ CREATE TABLE "local" (
 CREATE TABLE tarefa (
     id serial PRIMARY KEY NOT NULL,
     nome character varying(75) NOT NULL,   
-    fk_convidado_id integer NOT NULL,
     fk_local_id integer NOT NULL,
     data_hora timestamp NOT NULL,
-    duracao time NOT NULL
+    duracao character varying(5) NOT NULL
 );
 
-ALTER TABLE tarefa ADD CONSTRAINT fk_convidado_id FOREIGN KEY (fk_convidado_id) REFERENCES convidado(id) ON UPDATE CASCADE ON DELETE NO ACTION;
 ALTER TABLE tarefa ADD CONSTRAINT fk_local_id FOREIGN KEY (fk_local_id) REFERENCES local(id) ON UPDATE CASCADE ON DELETE NO ACTION;
+
+CREATE TABLE tarefa_convidado (
+    fk_tarefa_id integer NOT NULL,
+    fk_convidado_id integer NOT NULL
+);
+
+ALTER TABLE tarefa_convidado ADD CONSTRAINT fk_tarefa_id FOREIGN KEY (fk_tarefa_id) REFERENCES tarefa(id) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE tarefa_convidado ADD CONSTRAINT fk_convidado_id FOREIGN KEY (fk_convidado_id) REFERENCES convidado(id) ON UPDATE CASCADE ON DELETE NO ACTION;
